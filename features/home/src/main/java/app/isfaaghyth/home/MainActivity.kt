c851b824d415998f0fa02a67619ea2591f72d131
isfaaghyth <isfaaghyth@gmail.com>
2020-10-19 21:05:16 +0700

    created sample compose

---
+++ features/home/src/main/java/app/isfaaghyth/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.isfaaghyth.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ features/home/src/main/java/app/isfaaghyth/home/MainContent.kt
@@ -0,0 +1,16 @@
+package app.isfaaghyth.home
+
+import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+import androidx.ui.tooling.preview.Preview
+
+@Composable
+fun Greeting(name: String) {
+    Text("Hello $name")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Isfha")
+}
